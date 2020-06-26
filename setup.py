8c5ee1a1578d7222fce4790e18ac771468839ef6
Martin Hartl <g@inlart.com>
2020-06-27 01:01:19 +0200

    Add an initial setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="rgbmon",
+    version="0.0.1",
+    author="inlart",
+    author_email="g@inlart.com",
+    description="System monitoring tool that colors your hardware",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/inlart/RGBmon",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+    entry_points = {
+        'console_scripts': ['rgbmon=rgbmon.rgbmon:main'],
+    },
+)
