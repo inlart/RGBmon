88c44c372ea1153a26f7c5bc8857abea3608a356
Martin Hartl <g@inlart.com>
2020-07-17 19:18:32 +0200

    Add dot converter

---
+++ rgbmon/converter/dot.py
@@ -0,0 +1,28 @@
+import math
+
+import core.utils
+
+class Converter:
+    def __init__(self, config, backend):
+        # TODO: check if colors == 2
+        self.backend = backend
+        self.leds = backend.get_led_list(config["leds"])
+        self.default = core.utils.Color.from_string(config["colors"][0])
+        self.filled = core.utils.Color.from_string(config["colors"][1])
+
+
+    def convert(self, value):
+        segment_size = 100.0 / len(self.leds)
+        segment = math.floor(value / segment_size)
+
+        if(segment == segment_size):
+            segment = segment_size - 1
+
+        out = []
+        for i in range(len(self.leds)):
+            data = self.default
+            if i == segment:
+                data = self.filled
+
+            out.append((self.leds[i], data))
+        return out
