38c770cbafd2844119235d9efa3ca1c1dc1e2b2e
Martin Hartl <g@inlart.com>
2020-07-21 17:36:22 +0200

    Add rainbow converter

---
+++ rgbmon/converter/rainbow.py
@@ -0,0 +1,23 @@
+import math
+
+import core.utils
+
+class Converter:
+    def __init__(self, config, backend):
+        self.backend = backend
+        self.leds = backend.get_led_list(config["leds"])
+
+    def convert(self, value):
+        num_segments = len(self.leds)
+        segment_size = 1.0 / num_segments
+        offset = value / 100
+
+        out = []
+        for i in range(len(self.leds)):
+            value = offset + i * segment_size
+            if value > 1.0:
+                value -= 1.0
+            data = core.utils.rgb_from_hsv(value, 1, 1)
+
+            out.append((self.leds[i], data))
+        return out
--- rgbmon/core/utils.py
+++ rgbmon/core/utils.py
@@ -1,5 +1,11 @@
+import colorsys
+
 def rgb_from_string(color):
     r, g, b = tuple(int(color[i:i+2], 16) for i in (0, 2, 4))
+    return (r, g, b)
+
+def rgb_from_hsv(h,s,v):
+    r, g, b = tuple(round(i * 255) for i in colorsys.hsv_to_rgb(h,s,v))
     return (r, g, b)
 
 def interpolate(start, end, v):
