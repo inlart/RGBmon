a81c0a6a24124e2e2abd97bf97a46ed715d2e2fe
Martin Hartl <g@inlart.com>
2020-11-14 23:01:52 +0100

    Add inverted colors

---
+++ rgbmon/color/invert.py
@@ -0,0 +1,17 @@
+import core.utils
+from core.colormanager import ColorManager
+
+def invert(value):
+    return 0xFF - value
+
+class Color:
+    def __init__(self, settings):
+        print(settings)
+        self.colors = ColorManager(settings["colors"])
+
+    def __getitem__(self, key):
+        inverted = map(invert, self.colors[key])
+        return inverted
+
+    def __len__(self):
+        return len(self.colors)
