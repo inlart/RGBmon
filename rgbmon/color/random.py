eca47a4d6620a7df33106f339c3dc9ce71264c70
Martin Hartl <g@inlart.com>
2020-11-16 18:09:36 +0100

    color: Add random color

---
+++ rgbmon/color/random.py
@@ -0,0 +1,37 @@
+import random
+import logging
+import time
+
+import core.utils
+
+log = logging.getLogger(__name__)
+
+def current_time():
+    return int(round(time.time() * 1000))
+
+def randomColor():
+    return tuple([random.randint(0, 0xff) for i in range(3)])
+
+class Color:
+    def __init__(self, settings):
+        self.interval = 1000
+        self.colorSize = 1
+        if settings and "interval" in settings:
+            self.interval = settings["interval"]
+        if settings and "size" in settings:
+            self.colorSize = settings["size"]
+        log.debug("Random color interval set to {}".format(self.interval))
+        log.debug("Random color size set to {}".format(self.colorSize))
+        self.setValue()
+
+    def setValue(self):
+        self.lastUpdate = current_time()
+        self.value = [randomColor() for i in range(self.colorSize)]
+
+    def __getitem__(self, key):
+        if current_time() - self.lastUpdate > self.interval:
+            self.setValue()
+        return self.value[key]
+
+    def __len__(self):
+        return self.colorSize
