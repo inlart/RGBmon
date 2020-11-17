da0e31a8b6467716c330bc72a506d39dcaf7c8f4
Martin Hartl <g@inlart.com>
2020-11-17 18:57:11 +0100

    tests: Add static effect test

---
+++ tests/effect/test_static.py
@@ -0,0 +1,55 @@
+import pytest
+import json
+from rgbmon.effect.static import Effect
+
+
+class Backend():
+    def get_led_list(self, _):
+        return range(4)
+
+@pytest.fixture()
+def effect():
+    backend = Backend()
+    config = json.loads('{"colors":["FF0000","00FF00"]}')
+    backend_config = json.loads('{"leds":[{"type": 1,"mode": "direct"}]}')
+    return Effect(config, backend, backend_config)
+
+class TestStatic:
+    def test_zero(self, effect):
+        color_list = effect.convert(0.0)
+        for i, color in color_list:
+            r, g, b = color
+            if i % 2 == 0:
+                assert(r == 255)
+                assert(g == 0)
+                assert(b == 0)
+            else:
+                assert(r == 0)
+                assert(g == 255)
+                assert(b == 0)
+
+    def test_half(self, effect):
+        color_list = effect.convert(50.0)
+        for i, color in color_list:
+            r, g, b = color
+            if i % 2 == 0:
+                assert(r == 255)
+                assert(g == 0)
+                assert(b == 0)
+            else:
+                assert(r == 0)
+                assert(g == 255)
+                assert(b == 0)
+
+    def test_full(self, effect):
+        color_list = effect.convert(50.0)
+        for i, color in color_list:
+            r, g, b = color
+            if i % 2 == 0:
+                assert(r == 255)
+                assert(g == 0)
+                assert(b == 0)
+            else:
+                assert(r == 0)
+                assert(g == 255)
+                assert(b == 0)
