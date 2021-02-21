95e0c02ef709dd84f935db15995ac0b4ad4c35a5
Martin Hartl <g@inlart.com>
2021-02-21 16:36:40 +0100

    tests: Add multicolor fill test

--- tests/effect/test_fill.py
+++ tests/effect/test_fill.py
@@ -15,6 +15,12 @@ def effect():
     backend_config = json.loads('{"leds":[{"type": 1,"mode": "direct"}]}')
     return Effect(config, backend, backend_config)
 
+@pytest.fixture()
+def multi_effect():
+    backend = Backend()
+    config = json.loads('{"colors":["FF0000","00FF00","0000FF"]}')
+    backend_config = json.loads('{"leds":[{"type": 1,"mode": "direct"}]}')
+    return Effect(config, backend, backend_config)
 
 class TestFill:
     def test_zero(self, effect):
@@ -75,3 +81,40 @@ class TestFill:
             assert(r == 0)
             assert(g == 255)
             assert(b == 0)
+
+    def test_multicolor_zero(self, multi_effect):
+        color_list = multi_effect.convert(0.0)
+        for i, color in color_list:
+            r, g, b = color
+            assert(r == 255)
+            assert(g == 0)
+            assert(b == 0)
+
+    def test_multicolor_quarter(self, multi_effect):
+        color_list = multi_effect.convert(25.0)
+        for i, color in color_list:
+            r, g, b = color
+            if i < 2:
+                assert(r == 0)
+                assert(g == 255)
+                assert(b == 0)
+            else:
+                assert(r == 255)
+                assert(g == 0)
+                assert(b == 0)
+
+    def test_multicolor_half(self, multi_effect):
+        color_list = multi_effect.convert(50.0)
+        for i, color in color_list:
+            r, g, b = color
+            assert(r == 0)
+            assert(g == 255)
+            assert(b == 0)
+
+    def test_multicolor_full(self, multi_effect):
+        color_list = multi_effect.convert(100.0)
+        for i, color in color_list:
+            r, g, b = color
+            assert(r == 0)
+            assert(g == 0)
+            assert(b == 255)
